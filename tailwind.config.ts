a22aa6d2636e7c75db404d965ab066d840ba2109
maxx2002 <max.wiranata@unitcode.org>
2024-07-01 13:28:23 +0700

    fonts configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,95 @@
 import type { Config } from "tailwindcss";
 
+// const config: Config = {
+//   content: [
+//     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [require("tailwindcss-animate")],
+// };
+
 const config: Config = {
+  darkMode: ["class"],
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        unit: {
+          black: {
+            100: "#1c1c1c",
+            90: "#2c2c2e",
+            80: "#3a3a3c",
+            60: "#48484a",
+          },
+          grey: {
+            40: "#8e8e93",
+            30: "#c7c7cc",
+            20: "#e5e5ea",
+            10: "#f8f9fc",
+          },
+          purple: {
+            60: "#4b1fff",
+            40: "#6a45ff",
+            20: "#9d85ff",
+            10: "#ECE8FF",
+          },
+          blue: {
+            60: "#2fadeb",
+            40: "#52b9eb",
+            20: "#8dcceb",
+          },
+          turquoise: {
+            60: "#29ccb4",
+            40: "#85e1cb",
+            20: "#bde1dd",
+          },
+        },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "pulse-slow": "pulse 5s linear infinite",
       },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
+
 export default config;
